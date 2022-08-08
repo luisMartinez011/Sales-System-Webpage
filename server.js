28f408bd47a1e922255fda7bcc1e739510a17fd2
Your Name <luis.martinez33@protonmail.com>
2022-08-08 18:39:54 -0500

    pre deploy a heroku

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/dotnetVentas'));
+app.use(express.static(__dirname + '/dist/dotnet-ventas'));
 
 // Send all requests to index.html
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/dotnetVentas/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/dotnet-ventas/index.html'));
 });
 
 // default Heroku port
